9f69deda56a467283af1425607fb565dbc7f3fc8
Driss Amri <driss.amri@telenet.be>
2018-12-06 01:02:11 +0100

    Remove X-Ray

--- documentation/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-gradle clean build
-sls deploy function -f create-label -l
--- documentation/invoke-sns.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-aws sns publish --topic-arn arn:aws:sns:eu-west-1:539106611526:serverless-architecture-prd-label-request-topic --message "d"
-
-
-
